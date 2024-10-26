c8ece291c47cb24ee2af8fdae8086385a8ed48e7
Gaurav Khadka <gauravkhadka111111@gmail.com>
2024-10-26 08:33:15 +0545

    Login page styled

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,53 +8,59 @@
     <link rel="stylesheet" href="{{url('frontend/css/login.css')}}">
 </head>
 <body>
-<div class="loginpageb">
+    <div class="login-page">
+        <div class="loginpageb">
+            <div class="loginpageinput">
+                <div class="wtnlogo">
+                    <img src="frontend/images/wtn.png" alt="">
+                </div>
+                <div class="loginh">
+                    <h1>Login</h1>
+                </div>
 
-<div class="loginh">
-    <h1>Login</h1>
-</div>
+                <!-- Session Status -->
+                <x-auth-session-status class="mb-4" :status="session('status')" />
 
-<!-- Session Status -->
-<x-auth-session-status class="mb-4" :status="session('status')" />
+                <form method="POST" action="{{ route('login') }}">
+                    @csrf
 
-<form method="POST" action="{{ route('login') }}">
-    @csrf
+                    <!-- Email Address -->
+                    <div class="loginemail">
+                        <x-input-label for="email" id="lemailn" :value="__('Email')" />
+                        <x-text-input id="lemail" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
+                        <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                    </div>
 
-    <!-- Email Address -->
-    <div class="loginemail">
-        <x-input-label for="email" id="lemailn" :value="__('Email')" />
-        <x-text-input id="lemail" class="block mt-1 w-full" type="email"  name="email" :value="old('email')" required autofocus autocomplete="username" />
-        <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                    <!-- Password -->
+                    <div class="loginpswd">
+                        <x-input-label for="password" id="lpasswdn" :value="__('Password')" />
+                        <x-text-input id="lpassword" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
+                        <x-input-error :messages="$errors->get('password')" class="mt-2" />
+                    </div>
+
+                    <!-- Remember Me -->
+                    <div class="rememberl">
+                        <label for="remember_me" class="reml">
+                            <input id="remlc" type="checkbox" name="remember">
+                            <span class="remtext">{{ __('Remember me') }}</span>
+                        </label>
+                    </div>
+
+                    <div class="forgtpslogin">
+                        @if (Route::has('password.request'))
+                            <!-- <a class="frgtpswd" href="{{ route('password.request') }}">
+                                {{ __('Forgot your password?') }}
+                            </a> -->
+                        @endif
+
+                        <x-primary-button class="llogin">
+                            {{ __('Log in') }}
+                        </x-primary-button>
+                    </div>
+                </form>
+            </div>
+        </div>
     </div>
-
-    <!-- Password -->
-    <div class="loginpswd">
-        <x-input-label for="password" id="lpasswdn" :value="__('Password')" />
-        <x-text-input id="lpassword" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
-        <x-input-error :messages="$errors->get('password')" class="mt-2" />
-    </div>
-
-    <!-- Remember Me -->
-    <div class="rememberl">
-        <label for="remember_me" class="reml">
-            <input id="remlc" type="checkbox" name="remember">
-            <span class="remtext">{{ __('Remember me') }}</span>
-        </label>
-    </div>
-
-    <div class="forgtpslogin">
-        @if (Route::has('password.request'))
-            <a class="frgtpswd" href="{{ route('password.request') }}">
-                {{ __('Forgot your password?') }}
-            </a>
-        @endif
-
-        <x-primary-button class="llogin">
-            {{ __('Log in') }}
-        </x-primary-button>
-    </div>
-</form>
-
-</div> 
 </body>
+
 </html>
--- storage/framework/views/5f8b44492cebe2f60c4f9ec92f528c31.php
+++ storage/framework/views/5f8b44492cebe2f60c4f9ec92f528c31.php
@@ -8,14 +8,18 @@
     <link rel="stylesheet" href="<?php echo e(url('frontend/css/login.css')); ?>">
 </head>
 <body>
-<div class="loginpageb">
+    <div class="login-page">
+        <div class="loginpageb">
+            <div class="loginpageinput">
+                <div class="wtnlogo">
+                    <img src="frontend/images/wtn.png" alt="">
+                </div>
+                <div class="loginh">
+                    <h1>Login</h1>
+                </div>
 
-<div class="loginh">
-    <h1>Login</h1>
-</div>
-
-<!-- Session Status -->
-<?php if (isset($component)) { $__componentOriginal7c1bf3a9346f208f66ee83b06b607fb5 = $component; } ?>
+                <!-- Session Status -->
+                <?php if (isset($component)) { $__componentOriginal7c1bf3a9346f208f66ee83b06b607fb5 = $component; } ?>
 <?php if (isset($attributes)) { $__attributesOriginal7c1bf3a9346f208f66ee83b06b607fb5 = $attributes; } ?>
 <?php $component = Illuminate\View\AnonymousComponent::resolve(['view' => 'components.auth-session-status','data' => ['class' => 'mb-4','status' => session('status')]] + (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag ? $attributes->all() : [])); ?>
 <?php $component->withName('auth-session-status'); ?>
@@ -36,12 +40,12 @@
 <?php unset($__componentOriginal7c1bf3a9346f208f66ee83b06b607fb5); ?>
 <?php endif; ?>
 
-<form method="POST" action="<?php echo e(route('login')); ?>">
-    <?php echo csrf_field(); ?>
+                <form method="POST" action="<?php echo e(route('login')); ?>">
+                    <?php echo csrf_field(); ?>
 
-    <!-- Email Address -->
-    <div class="loginemail">
-        <?php if (isset($component)) { $__componentOriginale3da9d84bb64e4bc2eeebaafabfb2581 = $component; } ?>
+                    <!-- Email Address -->
+                    <div class="loginemail">
+                        <?php if (isset($component)) { $__componentOriginale3da9d84bb64e4bc2eeebaafabfb2581 = $component; } ?>
 <?php if (isset($attributes)) { $__attributesOriginale3da9d84bb64e4bc2eeebaafabfb2581 = $attributes; } ?>
 <?php $component = Illuminate\View\AnonymousComponent::resolve(['view' => 'components.input-label','data' => ['for' => 'email','id' => 'lemailn','value' => __('Email')]] + (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag ? $attributes->all() : [])); ?>
 <?php $component->withName('input-label'); ?>
@@ -61,7 +65,7 @@
 <?php $component = $__componentOriginale3da9d84bb64e4bc2eeebaafabfb2581; ?>
 <?php unset($__componentOriginale3da9d84bb64e4bc2eeebaafabfb2581); ?>
 <?php endif; ?>
-        <?php if (isset($component)) { $__componentOriginal18c21970322f9e5c938bc954620c12bb = $component; } ?>
+                        <?php if (isset($component)) { $__componentOriginal18c21970322f9e5c938bc954620c12bb = $component; } ?>
 <?php if (isset($attributes)) { $__attributesOriginal18c21970322f9e5c938bc954620c12bb = $attributes; } ?>
 <?php $component = Illuminate\View\AnonymousComponent::resolve(['view' => 'components.text-input','data' => ['id' => 'lemail','class' => 'block mt-1 w-full','type' => 'email','name' => 'email','value' => old('email'),'required' => true,'autofocus' => true,'autocomplete' => 'username']] + (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag ? $attributes->all() : [])); ?>
 <?php $component->withName('text-input'); ?>
@@ -81,7 +85,7 @@
 <?php $component = $__componentOriginal18c21970322f9e5c938bc954620c12bb; ?>
 <?php unset($__componentOriginal18c21970322f9e5c938bc954620c12bb); ?>
 <?php endif; ?>
-        <?php if (isset($component)) { $__componentOriginalf94ed9c5393ef72725d159fe01139746 = $component; } ?>
+                        <?php if (isset($component)) { $__componentOriginalf94ed9c5393ef72725d159fe01139746 = $component; } ?>
 <?php if (isset($attributes)) { $__attributesOriginalf94ed9c5393ef72725d159fe01139746 = $attributes; } ?>
 <?php $component = Illuminate\View\AnonymousComponent::resolve(['view' => 'components.input-error','data' => ['messages' => $errors->get('email'),'class' => 'mt-2']] + (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag ? $attributes->all() : [])); ?>
 <?php $component->withName('input-error'); ?>
@@ -101,11 +105,11 @@
 <?php $component = $__componentOriginalf94ed9c5393ef72725d159fe01139746; ?>
 <?php unset($__componentOriginalf94ed9c5393ef72725d159fe01139746); ?>
 <?php endif; ?>
-    </div>
+                    </div>
 
-    <!-- Password -->
-    <div class="loginpswd">
-        <?php if (isset($component)) { $__componentOriginale3da9d84bb64e4bc2eeebaafabfb2581 = $component; } ?>
+                    <!-- Password -->
+                    <div class="loginpswd">
+                        <?php if (isset($component)) { $__componentOriginale3da9d84bb64e4bc2eeebaafabfb2581 = $component; } ?>
 <?php if (isset($attributes)) { $__attributesOriginale3da9d84bb64e4bc2eeebaafabfb2581 = $attributes; } ?>
 <?php $component = Illuminate\View\AnonymousComponent::resolve(['view' => 'components.input-label','data' => ['for' => 'password','id' => 'lpasswdn','value' => __('Password')]] + (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag ? $attributes->all() : [])); ?>
 <?php $component->withName('input-label'); ?>
@@ -125,7 +129,7 @@
 <?php $component = $__componentOriginale3da9d84bb64e4bc2eeebaafabfb2581; ?>
 <?php unset($__componentOriginale3da9d84bb64e4bc2eeebaafabfb2581); ?>
 <?php endif; ?>
-        <?php if (isset($component)) { $__componentOriginal18c21970322f9e5c938bc954620c12bb = $component; } ?>
+                        <?php if (isset($component)) { $__componentOriginal18c21970322f9e5c938bc954620c12bb = $component; } ?>
 <?php if (isset($attributes)) { $__attributesOriginal18c21970322f9e5c938bc954620c12bb = $attributes; } ?>
 <?php $component = Illuminate\View\AnonymousComponent::resolve(['view' => 'components.text-input','data' => ['id' => 'lpassword','class' => 'block mt-1 w-full','type' => 'password','name' => 'password','required' => true,'autocomplete' => 'current-password']] + (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag ? $attributes->all() : [])); ?>
 <?php $component->withName('text-input'); ?>
@@ -145,7 +149,7 @@
 <?php $component = $__componentOriginal18c21970322f9e5c938bc954620c12bb; ?>
 <?php unset($__componentOriginal18c21970322f9e5c938bc954620c12bb); ?>
 <?php endif; ?>
-        <?php if (isset($component)) { $__componentOriginalf94ed9c5393ef72725d159fe01139746 = $component; } ?>
+                        <?php if (isset($component)) { $__componentOriginalf94ed9c5393ef72725d159fe01139746 = $component; } ?>
 <?php if (isset($attributes)) { $__attributesOriginalf94ed9c5393ef72725d159fe01139746 = $attributes; } ?>
 <?php $component = Illuminate\View\AnonymousComponent::resolve(['view' => 'components.input-error','data' => ['messages' => $errors->get('password'),'class' => 'mt-2']] + (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag ? $attributes->all() : [])); ?>
 <?php $component->withName('input-error'); ?>
@@ -165,25 +169,25 @@
 <?php $component = $__componentOriginalf94ed9c5393ef72725d159fe01139746; ?>
 <?php unset($__componentOriginalf94ed9c5393ef72725d159fe01139746); ?>
 <?php endif; ?>
-    </div>
+                    </div>
 
-    <!-- Remember Me -->
-    <div class="rememberl">
-        <label for="remember_me" class="reml">
-            <input id="remlc" type="checkbox" name="remember">
-            <span class="remtext"><?php echo e(__('Remember me')); ?></span>
-        </label>
-    </div>
+                    <!-- Remember Me -->
+                    <div class="rememberl">
+                        <label for="remember_me" class="reml">
+                            <input id="remlc" type="checkbox" name="remember">
+                            <span class="remtext"><?php echo e(__('Remember me')); ?></span>
+                        </label>
+                    </div>
 
-    <div class="forgtpslogin">
-        <?php if(Route::has('password.request')): ?>
-            <a class="frgtpswd" href="<?php echo e(route('password.request')); ?>">
-                <?php echo e(__('Forgot your password?')); ?>
+                    <div class="forgtpslogin">
+                        <?php if(Route::has('password.request')): ?>
+                            <!-- <a class="frgtpswd" href="<?php echo e(route('password.request')); ?>">
+                                <?php echo e(__('Forgot your password?')); ?>
 
-            </a>
-        <?php endif; ?>
+                            </a> -->
+                        <?php endif; ?>
 
-        <?php if (isset($component)) { $__componentOriginald411d1792bd6cc877d687758b753742c = $component; } ?>
+                        <?php if (isset($component)) { $__componentOriginald411d1792bd6cc877d687758b753742c = $component; } ?>
 <?php if (isset($attributes)) { $__attributesOriginald411d1792bd6cc877d687758b753742c = $attributes; } ?>
 <?php $component = Illuminate\View\AnonymousComponent::resolve(['view' => 'components.primary-button','data' => ['class' => 'llogin']] + (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag ? $attributes->all() : [])); ?>
 <?php $component->withName('primary-button'); ?>
@@ -193,9 +197,9 @@
 <?php $attributes = $attributes->except(\Illuminate\View\AnonymousComponent::ignoredParameterNames()); ?>
 <?php endif; ?>
 <?php $component->withAttributes(['class' => 'llogin']); ?>
-            <?php echo e(__('Log in')); ?>
+                            <?php echo e(__('Log in')); ?>
 
-         <?php echo $__env->renderComponent(); ?>
+                         <?php echo $__env->renderComponent(); ?>
 <?php endif; ?>
 <?php if (isset($__attributesOriginald411d1792bd6cc877d687758b753742c)): ?>
 <?php $attributes = $__attributesOriginald411d1792bd6cc877d687758b753742c; ?>
@@ -205,9 +209,11 @@
 <?php $component = $__componentOriginald411d1792bd6cc877d687758b753742c; ?>
 <?php unset($__componentOriginald411d1792bd6cc877d687758b753742c); ?>
 <?php endif; ?>
+                    </div>
+                </form>
+            </div>
+        </div>
     </div>
-</form>
-
-</div> 
 </body>
+
 </html><?php /**PATH C:\xampp\htdocs\Oct-25-Project-Management\resources\views/auth/login.blade.php ENDPATH**/ ?>
